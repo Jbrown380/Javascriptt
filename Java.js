9eb24a4b856bf74a8aa164bdb3cac2c9dcfc7186
Jaden Brown <jadenmb01@gmail.com>
2021-09-09 18:06:21 -0400

    Initial Commit

---
+++ Java.js
@@ -0,0 +1,12 @@
+let age = 20;
+
+ let firstName = "Jaden";
+  let lastName = " Brown";
+
+alert("My name is " + firstName + lastName);
+
+alert("I am " + age + " years old")
+
+
+
+  console.log("I am having a ton of fun in this class!")
